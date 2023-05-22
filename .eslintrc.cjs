7b29a562dd9656eda273aeec0dd4392946228285
Jonathan Terrell <terrell.jm@gmail.com>
2023-05-22 18:47:31 +0200

    v0.0.137

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,16 +12,17 @@
 
 module.exports = {
     env: { browser: true, node: true },
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+    extends: ['eslint:recommended'],
+    globals: {},
     overrides: [
-        { files: ['*.js'], parser: 'babel-eslint' } // Use the Babel parser for JavaScript files.
+        { files: ['*.js'], parser: 'espree' }, // Use the default parser for JavaScript files.
+        { files: ['**/*.ts'], extends: ['plugin:@typescript-eslint/recommended', 'plugin:@typescript-eslint/recommended-requiring-type-checking'] }
     ],
     parser: '@typescript-eslint/parser',
-    parserOptions: { ecmaVersion: 'latest' },
+    parserOptions: { ecmaVersion: 'latest', sourceType: 'module', project: './tsconfig.json', tsconfigRootDir: '.' },
     plugins: ['@typescript-eslint', 'import'],
     root: true,
     rules: {
-        'no-unused-vars': 'warn',
         'sort-imports': ['warn', { allowSeparatedGroups: true, ignoreCase: true, memberSyntaxSortOrder: ['none', 'all', 'single', 'multiple'] }]
     }
 };
