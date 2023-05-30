d1a429d019d465c68f103e7d410082702added76
Jonathan Terrell <terrell.jm@gmail.com>
2023-05-30 22:46:46 +0200

    v0.0.199

--- src/sourceView.ts
+++ src/sourceView.ts
@@ -41,7 +41,7 @@ export interface SourceViewPreview {
     encodingConfidenceLevel?: number;
     encodingId?: string;
     hasHeaderLine?: boolean;
-    lineDelimiter: Buffer;
+    lineDelimiter?: Buffer;
     lineDelimiterId?: string;
     linesToSkipBeforeHeader?: number;
     linesToSkipAfterHeader?: number;
@@ -55,7 +55,7 @@ export interface SourceViewPreview {
     skipLinesWithErrors?: boolean;
     text?: string;
     totalSize?: number;
-    valueDelimiter: Buffer;
+    valueDelimiter?: Buffer;
     valueDelimiterId?: ValueDelimiterId;
     valueTrimMethodId?: string;
 }
