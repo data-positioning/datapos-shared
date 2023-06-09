6e9dd24b7d2b8e06f1e7d8204d55f91aa0c21f17
Jonathan Terrell <terrell.jm@gmail.com>
2023-06-09 12:37:14 +0200

    v0.0.281

--- src/index.ts
+++ src/index.ts
@@ -103,7 +103,7 @@ export type { UsageKitConfig } from './usageKit';
 
 export type { ViewTemplateConfig } from './viewTemplate';
 
-export { AbortError, ConnectorError, ContextualError, EngineError, FetchResponseError, type SerialisedErrorData } from './errors';
+export { AbortError, ConnectorError, ContextualError, EngineError, FetchResponseError, type SerialisedErrorData, WorkerError } from './errors';
 
 export {
     convertODataTypeToDataType,
