b283ba98f6217c9e84c3025ed17e8a43adf20b8b
Jonathan Terrell <terrell.jm@gmail.com>
2023-06-23 23:51:33 +0200

    v0.0.333

--- gruntfile.js
+++ gruntfile.js
@@ -1,6 +1,7 @@
 // Dependencies - Operations
 const {
     auditDependencies,
+    buildWithVite,
     checkDependencies,
     identifyLicenses,
     logNotImplementedMessage,
@@ -16,6 +17,9 @@ module.exports = (grunt) => {
     // Register local tasks.
     grunt.registerTask('auditDependencies', function () {
         auditDependencies(grunt, this);
+    });
+    grunt.registerTask('buildWithVite', function () {
+        buildWithVite(grunt, this);
     });
     grunt.registerTask('checkDependencies', function () {
         checkDependencies(grunt, this);
@@ -42,14 +46,14 @@ module.exports = (grunt) => {
 
     // Register common repository management tasks. These tasks are all invoked by VSCode keyboard shortcuts identified in the comments.
     grunt.registerTask('audit', ['auditDependencies']); // alt+ctrl+shift+a.
-    grunt.registerTask('build', ['shell:build']); // alt+ctrl+shift+b.
+    grunt.registerTask('build', ['buildWithVite']); // alt+ctrl+shift+b.
     grunt.registerTask('check', ['checkDependencies']); // alt+ctrl+shift+c.≤
     grunt.registerTask('document', ['identifyLicenses']); // alt+ctrl+shift+d.
     grunt.registerTask('format', ['logNotImplementedMessage:Format']); // alt+ctrl+shift+f.
     grunt.registerTask('lint', ['lintCode']); // alt+ctrl+shift+l.
     grunt.registerTask('migrate', ['migrateDependencies']); // alt+ctrl+shift+m.
     grunt.registerTask('publish', ['publishPackageToNPM']); // alt+ctrl+shift+p.
-    grunt.registerTask('release', ['syncRepoWithGithub', 'build', 'publishPackageToNPM']); // alt+ctrl+shift+r.
+    grunt.registerTask('release', ['syncRepoWithGithub', 'buildWithVite', 'publishPackageToNPM']); // alt+ctrl+shift+r.
     grunt.registerTask('synchronise', ['syncRepoWithGithub']); // alt+ctrl+shift+s.
     grunt.registerTask('test', ['logNotImplementedMessage:Test']); // alt+ctrl+shift+t.
     grunt.registerTask('update', ['updateDataPosDependencies']); // alt+ctrl+shift+u.
