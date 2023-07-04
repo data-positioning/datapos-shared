6b8b5d1e2529ffbb7299e7fb5a99bc74c7432c16
Jonathan Terrell <terrell.jm@gmail.com>
2023-07-04 23:18:48 +0200

    v0.1.366

--- dist copy/types/usageKit.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Component, ComponentConfig } from './component';
-export interface UsageKit extends Component {
-    placeholder?: string;
-}
-export interface UsageKitConfig extends ComponentConfig {
-    placeholder?: string;
-}
