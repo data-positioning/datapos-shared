815b12c44553a0584ac38b9a3993ace46ddd9f50
Jonathan Terrell <terrell.jm@gmail.com>
2023-07-15 19:51:03 +0200

    v0.1.371

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-// Declarations - Component
+// Components
 export { type ComponentConfig, ComponentTypeId, getComponentStatus } from './component';
 
-// Declarations - Component - Connector
+// Components - Connector
 export { ConnectorAuthMethodId, type ConnectorCallbackData, type ConnectorConfig, ConnectorUsageId } from './connector/index';
 export type {
     DataConnector,
@@ -16,11 +16,11 @@ export type {
 } from './connector/dataConnector';
 export type {} from './nodeConnector';
 
-// Declarations - Component - Connection
+// Components - Connection
 export type { ConnectionConfig, ConnectionEntryDrilldownResult, ConnectionEntry, ConnectionEntryPreview } from './connection';
 export { ConnectionEntryPreviewTypeId, ConnectionEntryTypeId, FieldStorageTypeId } from './connection';
 
-// Declarations - Component - Context
+// Components - Context
 export type {
     ContextConfig,
     ContextFocusReference,
@@ -35,19 +35,19 @@ export type {
     ViewConfig
 } from './context';
 
-// Declarations - Component - Event Query
-export type {} from './eventQuery';
+// Components - Event Query
+export type { EventQueryConfig } from './eventQuery';
 
-// Declarations - Component - Source View
+//  Components - Source View
 export type { SourceViewConfig, SourceViewContentAudit, SourceViewPreview, SourceViewRelationshipsAudit } from './sourceView';
 export { DataFormatId, getDataFormats, getValueDelimiters, ValueDelimiterId } from './sourceView';
 export { PreviewColumn } from './sourceView/PreviewColumn';
 export { ContentAuditColumn } from './sourceView/ContentAuditColumn';
 
-// Declarations - Component - Usage Kit
+// Components - Usage Kit
 export type { UsageKitConfig } from './usageKit';
 
-// Declarations - Error
+// Errors
 export {
     AbortError,
     BackendContextError,
