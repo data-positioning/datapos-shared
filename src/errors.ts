b6e57f6d4e8194a9612c95a6587bdec157fe04c6
Jonathan Terrell <terrell.jm@gmail.com>
2024-05-16 18:03:03 +0200

    v0.1.509

--- src/errors.ts
+++ src/errors.ts
@@ -6,94 +6,6 @@ export interface SerialisedErrorData {
     stack?: string;
 }
 
-// Errors - Abort
-export class AbortError extends Error {
-    constructor(message: string) {
-        super(message);
-        this.name = 'AbortError';
-    }
-}
-
-// Errors - Backend
-export class BackendError extends Error {
-    context?: string;
-    notes?: string;
-    constructor(message: string, context?: string, cause?: unknown, notes?: string) {
-        super(message);
-        this.name = 'BackendError';
-        this.context = context;
-        this.cause = cause;
-        this.notes = notes;
-    }
-}
-
-// // Errors - Connector
-// export class ConnectorError extends Error {
-//     notes?: string;
-//     constructor(message: string, cause?: unknown, notes?: string) {
-//         super(message);
-//         this.name = 'ConnectorError';
-//         this.cause = cause;
-//         this.notes = notes;
-//     }
-// }
-
-// Errors - Engine
-export class EngineError extends Error {
-    notes?: string;
-    constructor(message: string, cause?: unknown, notes?: string) {
-        super(message);
-        this.name = 'EngineError';
-        this.cause = cause;
-        this.notes = notes;
-    }
-}
-
-// // Errors - Engine Core
-// export class EngineCoreError extends Error {
-//     notes?: string;
-//     originalName: string;
-//     constructor(message: string, originalName: string, notes?: string) {
-//         super(message);
-//         this.name = 'EngineCoreError';
-//         this.originalName = originalName;
-//         this.notes = notes;
-//     }
-// }
-
-// Errors - Engine Worker
-export class EngineWorkerError extends Error {
-    notes?: string;
-    constructor(cause?: unknown, notes?: string) {
-        super('Engine worker error wrapper.');
-        this.name = 'EngineWorkerError';
-        this.cause = cause;
-        this.notes = notes;
-    }
-}
-
-// // Errors - Fetch
-// export class FetchError extends Error {
-//     notes?: string;
-//     constructor(message: string, cause?: unknown, notes?: string) {
-//         super(message);
-//         this.name = 'FetchError';
-//         this.cause = cause;
-//         this.notes = notes;
-//     }
-// }
-
-// // Errors - Frontend
-// export class FrontendError extends Error {
-//     context?: string;
-//     constructor(message: string, context?: string, cause?: unknown) {
-//         super(message);
-//         this.name = 'FrontendError';
-//         this.context = context;
-//         this.cause = cause;
-//     }
-// }
-
 export class DataPosError extends Error {
     context?: string;
     data?: Record<string, unknown>;
@@ -108,10 +20,38 @@ export class DataPosError extends Error {
     }
 }
 
+export class AbortError extends DataPosError {
+    constructor(message: string, context: string, notes?: string, data?: Record<string, unknown>, cause?: unknown) {
+        super(message, context, notes, data, cause);
+        this.name = 'AbortError';
+    }
+}
+
+export class BackendError extends DataPosError {
+    constructor(message: string, context: string, notes?: string, data?: Record<string, unknown>, cause?: unknown) {
+        super(message, context, notes, data, cause);
+        this.name = 'BackendError';
+    }
+}
+
 export class ConnectorError extends DataPosError {
     constructor(message: string, context: string, notes?: string, data?: Record<string, unknown>, cause?: unknown) {
         super(message, context, notes, data, cause);
         this.name = 'ConnectorError';
+    }
+}
+
+export class EngineError extends DataPosError {
+    constructor(message: string, context: string, notes?: string, data?: Record<string, unknown>, cause?: unknown) {
+        super(message, context, notes, data, cause);
+        this.name = 'EngineError';
+    }
+}
+
+export class EngineWorkerError extends DataPosError {
+    constructor(message: string, context: string, notes?: string, data?: Record<string, unknown>, cause?: unknown) {
+        super(message, context, notes, data, cause);
+        this.name = 'EngineWorkerError';
     }
 }
 
