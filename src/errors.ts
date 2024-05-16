b0cecc4d1f555e9309a21f1c22b8b68c92a3a707
Jonathan Terrell <terrell.jm@gmail.com>
2024-05-16 17:04:02 +0200

    v0.1.506

--- src/errors.ts
+++ src/errors.ts
@@ -17,57 +17,69 @@ export class AbortError extends Error {
 // Errors - Backend
 export class BackendError extends Error {
     context?: string;
-    constructor(message: string, context?: string, cause?: unknown) {
+    notes?: string;
+    constructor(message: string, context?: string, cause?: unknown, notes?: string) {
         super(message);
         this.name = 'BackendError';
         this.context = context;
         this.cause = cause;
+        this.notes = notes;
     }
 }
 
-// Errors - Connector
-export class ConnectorError extends Error {
-    constructor(message: string, cause?: unknown) {
-        super(message);
-        this.name = 'ConnectorError';
-        this.cause = cause;
-    }
-}
+// // Errors - Connector
+// export class ConnectorError extends Error {
+//     notes?: string;
+//     constructor(message: string, cause?: unknown, notes?: string) {
+//         super(message);
+//         this.name = 'ConnectorError';
+//         this.cause = cause;
+//         this.notes = notes;
+//     }
+// }
 
 // Errors - Engine
 export class EngineError extends Error {
-    constructor(message: string, cause?: unknown) {
+    notes?: string;
+    constructor(message: string, cause?: unknown, notes?: string) {
         super(message);
         this.name = 'EngineError';
         this.cause = cause;
+        this.notes = notes;
     }
 }
 
-// Errors - Engine Core
-export class EngineCoreError extends Error {
-    originalName: string;
-    constructor(message: string, originalName: string) {
-        super(message);
-        this.name = 'EngineCoreError';
-        this.originalName = originalName;
-    }
-}
+// // Errors - Engine Core
+// export class EngineCoreError extends Error {
+//     notes?: string;
+//     originalName: string;
+//     constructor(message: string, originalName: string, notes?: string) {
+//         super(message);
+//         this.name = 'EngineCoreError';
+//         this.originalName = originalName;
+//         this.notes = notes;
+//     }
+// }
 
 // Errors - Engine Worker
 export class EngineWorkerError extends Error {
-    constructor(cause?: unknown) {
+    notes?: string;
+    constructor(cause?: unknown, notes?: string) {
         super('Engine worker error wrapper.');
         this.name = 'EngineWorkerError';
         this.cause = cause;
+        this.notes = notes;
     }
 }
 
 // Errors - Fetch
 export class FetchError extends Error {
-    constructor(message: string, cause?: unknown) {
+    notes?: string;
+    constructor(message: string, cause?: unknown, notes?: string) {
         super(message);
         this.name = 'FetchError';
         this.cause = cause;
+        this.notes = notes;
     }
 }
 
@@ -93,6 +105,13 @@ export class DataPosError extends Error {
         this.notes = notes;
         this.data = data;
         this.cause = cause;
+    }
+}
+
+export class ConnectorError extends DataPosError {
+    constructor(message: string, context: string, notes?: string, data?: Record<string, unknown>, cause?: unknown) {
+        super(message, context, notes, data, cause);
+        this.name = 'ConnectorError';
     }
 }
 
