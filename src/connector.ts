e6cccfb7189d563166f2068ad7ab60f77e5aba39
Jonathan Terrell <jonathanterrell@Jonathans-Mac-mini.local>
2025-08-26 22:40:31 +0200

    v0.3.15

--- src/connector.ts
+++ src/connector.ts
@@ -58,6 +58,7 @@ export interface ConnectorImplementation {
     maxConnectionCount?: number;
     params?: Record<string, string>[];
 }
+export type LocaleConnectorConfig = Omit<ConnectorConfig, 'label' | 'description'> & { label: string; description: string };
 
 // Interfaces/Types - Connector Operator Settings
 export interface ConnectorOperationSettings {
