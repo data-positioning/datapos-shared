307dff7ebadf09dea965cdd3b61ff3184dafb5d9
Jonathan Terrell <jonathanterrell@Jonathans-Mac-mini.local>
2025-11-06 13:11:14 +0100

    v0.3.119

--- src/devUtilities/index.ts
+++ src/devUtilities/index.ts
@@ -7,7 +7,7 @@ import type { PackageJson } from 'type-fest';
 import { promises as fs } from 'fs';
 
 // Dependencies - Framework.
-import type { ConnectorModuleConfig } from '@/module';
+import type { ConnectorModuleConfig, ConnectorModuleInterface } from '@/module';
 
 // Operations - Build connector configuration.
 export async function buildConnectorConfig() {
@@ -19,9 +19,13 @@ export async function buildConnectorConfig() {
         const regex = /^\s{4}(?:async\s+)?(private\s+)?(?:public\s+|protected\s+)?([A-Za-z_]\w*)\s*\(/gm;
         const operations = [...indexCode.matchAll(regex)]
             .filter((m) => !m[1] && m[2] !== 'constructor') // m[1] is 'private ' if present.
-            .map((m) => m[2]);
+            .map((m) => m[2]) as ConnectorModuleInterface[];
 
-        await fs.writeFile('configNEW.json', JSON.stringify({ ...configJSON, id: packageJSON.name, operations, version: packageJSON.version }, undefined, 4), 'utf8');
+        if (packageJSON.name) configJSON.id = packageJSON.name;
+        configJSON.operations = operations;
+        if (packageJSON.version) configJSON.version = packageJSON.version;
+
+        await fs.writeFile('configNEW.json', JSON.stringify(configJSON, undefined, 4), 'utf8');
     } catch (error) {
         console.warn('Error building connector configuration.', error);
     }
