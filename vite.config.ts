2c51b906d388242a5c718ec9899eb603d67fd804
Jonathan Terrell <jonathanterrell@Jonathans-Mac-mini.local>
2025-11-06 18:57:30 +0100

    v0.3.135

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
         },
         target: 'ESNext',
         rollupOptions: {
-            external: ['exec', 'fs', 'promises', 'promisify']
+            external: ['exec', 'fs', 'promises']
         }
     },
     plugins: [dts({ outDir: 'dist/types' })],
