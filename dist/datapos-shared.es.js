0ec94117b82228fb72b623a6f2cf0f6c46a4b1d7
Jonathan Terrell <jonathanterrell@Jonathans-Mac-mini.local>
2025-11-10 12:51:48 +0100

    v0.3.170

--- dist/datapos-shared.es.js
+++ dist/datapos-shared.es.js
@@ -1,100 +1,97 @@
-const F = ["createObject", "dropObject", "removeRecords", "upsertRecords"], j = ["findObject", "getRecord", "listNodes", "previewObject", "retrieveRecords"];
-function U() {
+const M = ["createObject", "dropObject", "removeRecords", "upsertRecords"], k = ["findObject", "getRecord", "listNodes", "previewObject", "retrieveRecords"];
+function F() {
   function e(t, a, r) {
     r.textContent = "Cytoscape.js diagram goes here...";
   }
   return { render: e };
 }
-function B() {
+function U() {
   function e(t, a, r) {
     r.textContent = "values table goes here...";
   }
   return { render: e };
 }
-const O = "https://cdn.jsdelivr.net/npm/highcharts@11.4.3/es-modules/masters/", x = "highcharts";
+const N = "https://cdn.jsdelivr.net/npm/highcharts@11.4.3/es-modules/masters/", x = "highcharts";
 let b, T = !1;
-function P() {
-  async function e(n, s, m, h) {
+function j() {
+  async function e(n, s, u, h) {
     await r();
     const l = [];
     for (const c of s.data.measures)
       l.push({ type: n.options.highchartsType, name: c.name, data: c.data });
-    const p = {
+    const g = {
       chart: { type: n.options.highchartsType },
       plotOptions: { series: { borderColor: "#333" } },
       series: l,
       title: { text: s.title.text },
       xAxis: { categories: s.data.categoryLabels },
       yAxis: { title: { text: s.data.name } }
-    }, u = b.chart(m, p, h);
-    return { chart: u, resize: () => u.reflow(), vendorId: x };
+    }, m = b.chart(u, g, h);
+    return { chart: m, resize: () => m.reflow(), vendorId: x };
   }
-  async function t(n, s, m, h) {
+  async function t(n, s, u, h) {
     await Promise.all([r(), o()]);
     const l = [];
     for (const c of s.data.measures)
       l.push({ type: n.options.highchartsType, name: c.name, data: c.data });
-    const p = {
+    const g = {
       chart: { polar: !0 },
       plotOptions: { series: { borderColor: "#333" } },
       series: l,
       title: { text: s.title.text },
       xAxis: { categories: s.data.categoryLabels },
       yAxis: { title: { text: s.data.name } }
-    }, u = b.chart(m, p, h);
-    return { chart: u, resize: () => u.reflow(), vendorId: x };
+    }, m = b.chart(u, g, h);
+    return { chart: m, resize: () => m.reflow(), vendorId: x };
   }
-  async function a(n, s, m, h) {
+  async function a(n, s, u, h) {
     await Promise.all([r(), o()]);
-    const l = [], p = [];
+    const l = [], g = [];
     for (let f = 0; f < s.data.measures[0].data.length; f++)
-      p.push([s.data.measures[0].data[f][0], s.data.measures[1].data[f][0]]);
-    l.push({ type: n.options.highchartsType, name: "Unknown", data: p });
-    const u = {
+      g.push([s.data.measures[0].data[f][0], s.data.measures[1].data[f][0]]);
+    l.push({ type: n.options.highchartsType, name: "Unknown", data: g });
+    const m = {
       chart: { type: n.options.highchartsType, inverted: n.options.inverted },
       plotOptions: { series: { borderColor: "#333" } },
       series: l,
       title: { text: s.title.text },
       xAxis: { categories: s.data.categoryLabels },
       yAxis: { title: { text: s.data.name } }
-    }, c = b.chart(m, u, h);
+    }, c = b.chart(u, m, h);
     return { chart: c, resize: () => c.reflow(), vendorId: x };
   }
   async function r() {
     if (b) return;
-    const n = "https://cdn.jsdelivr.net/npm/highcharts@11.4.3/es-modules/masters/", s = `${n}highcharts.src.js`, m = `${n}modules/accessibility.src.js`;
+    const n = "https://cdn.jsdelivr.net/npm/highcharts@11.4.3/es-modules/masters/", s = `${n}highcharts.src.js`, u = `${n}modules/accessibility.src.js`;
     b = (await import(
       /* @vite-ignore */
       s
     )).default, await import(
       /* @vite-ignore */
-      m
+      u
     );
   }
   async function o() {
     if (T) return;
-    await import(`${O}highcharts-more.src.js`), T = !0;
+    await import(`${N}highcharts-more.src.js`), T = !0;
   }
   return { renderCartesianChart: e, renderPolarChart: t, renderRangeChart: a };
 }
-const S = 4, A = `https://cdn.jsdelivr.net/npm/micromark@${S}/+esm`, N = 1, C = `https://cdn.jsdelivr.net/npm/prismjs@${N}/+esm`;
-let I, w;
-function W() {
+const S = 4, A = `https://cdn.jsdelivr.net/npm/micromark@${S}/+esm`;
+let I, D;
+function B() {
   async function e(a, r, o) {
     await t(), o.textContent = "Micromark & Prism content goes here...";
   }
   async function t() {
-    I && w || (I = await import(
+    I && D || (I = await import(
       /* @vite-ignore */
       A
-    ), w = await import(
-      /* @vite-ignore */
-      C
-    ), console.log("micromarkModule", I), console.log("prismModule", w));
+    ), console.log("micromarkModule", I), console.log("prismModule", D));
   }
   return { render: e };
 }
-const H = 0, z = (e) => e, V = () => Date.now(), X = {
+const H = 0, P = (e) => e, W = () => Date.now(), z = {
   cartesian_areaLine: { categoryId: "cartesian", typeId: "areaLine", label: { "en-gb": "Area Line" }, options: { highchartsType: "area" } },
   cartesian_areaSpline: { categoryId: "cartesian", typeId: "areaSpline", label: { "en-gb": "Area Spline" }, options: { highchartsType: "area" } },
   cartesian_bar: { categoryId: "cartesian", typeId: "bar", label: { "en-gb": "Bar" }, options: { highchartsType: "bar" } },
@@ -113,63 +110,63 @@ const H = 0, z = (e) => e, V = () => Date.now(), X = {
   streamgraph: { categoryId: "streamgraph", label: { "en-gb": "Streamgraph" }, options: {} },
   values: { categoryId: "values", label: { "en-gb": "Values" }, options: {} }
 };
-class D extends Error {
+class w extends Error {
   locator;
   constructor(t, a, r) {
     super(t, r), this.name = "DataPosError", this.locator = a, Error.captureStackTrace?.(this, new.target);
   }
 }
-class g extends D {
+class p extends w {
   constructor(t, a, r) {
     super(t, a, r), this.name = "ApplicationError";
   }
 }
-class G extends g {
+class V extends p {
   constructor(t, a, r) {
     super(t, a, r), this.name = "APIError";
   }
 }
-class K extends g {
+class X extends p {
   constructor(t, a, r) {
     super(t, a, r), this.name = "EngineError";
   }
 }
-class v extends g {
+class v extends p {
   body;
   constructor(t, a, r, o) {
     super(t, a, o), this.name = "FetchError", this.body = r;
   }
 }
-class _ extends g {
+class O extends p {
   componentName;
   info;
   constructor(t, a, r, o, n) {
     super(t, a, n), this.name = "VueHandledError", this.info = r, this.componentName = o;
   }
 }
-class J extends g {
+class G extends p {
   constructor(t, a, r) {
     super(t, a, r), this.name = "WindowHandledRuntimeError";
   }
 }
-class q extends g {
+class K extends p {
   constructor(t, a, r) {
     super(t, a, r), this.name = "WindowHandledPromiseRejectionError";
   }
 }
-class Q extends D {
+class J extends w {
   constructor(t, a, r) {
     super(t, a, r), this.name = "OperationalError";
   }
 }
-async function Y(e, t, a) {
+async function q(e, t, a) {
   const r = `${t} Response status '${e.status}${e.statusText ? ` - ${e.statusText}` : ""}' received.`, o = await e.text();
   return new v(r, a, o);
 }
-function Z(e) {
+function Q(e) {
   return e.map((t) => t.message).join(" ");
 }
-function ee(e, t = "Unknown error.") {
+function Y(e, t = "Unknown error.") {
   if (e instanceof Error) return e;
   if (typeof e == "string") return new Error(e);
   try {
@@ -178,7 +175,7 @@ function ee(e, t = "Unknown error.") {
     return new Error(t);
   }
 }
-function re(e) {
+function Z(e) {
   const t = /* @__PURE__ */ new Set(), a = [];
   let r = e;
   for (; r && !t.has(r); ) {
@@ -186,9 +183,9 @@ function re(e) {
     let o;
     if (r instanceof v)
       o = { body: r.body, locator: r.locator, message: r.message, name: r.name, stack: r.stack }, r = r.cause;
-    else if (r instanceof _)
+    else if (r instanceof O)
       o = { componentName: r.componentName, info: r.info, locator: r.locator, message: r.message, name: r.name, stack: r.stack }, r = r.cause;
-    else if (r instanceof D)
+    else if (r instanceof w)
       o = { locator: r.locator, message: r.message, name: r.name, stack: r.stack }, r = r.cause;
     else if (r instanceof Error) {
       const n = r;
@@ -198,7 +195,7 @@ function re(e) {
   }
   return a;
 }
-const R = "en-US", y = {}, te = (e) => {
+const R = "en-US", y = {}, ee = (e) => {
   switch (e) {
     case "Edm.Binary":
       return "unknown";
@@ -237,12 +234,12 @@ const R = "en-US", y = {}, te = (e) => {
     default:
       return "unknown";
   }
-}, ae = (e) => {
+}, re = (e) => {
   if (e) {
     const t = e.lastIndexOf("/"), a = e.lastIndexOf(".", t > -1 ? t : e.length);
     return a > -1 ? e.substring(0, a) : e;
   }
-}, ne = (e) => {
+}, te = (e) => {
   if (e) {
     const t = e.lastIndexOf(".");
     if (t > -1) return e.substring(t + 1);
@@ -259,7 +256,7 @@ const R = "en-US", y = {}, te = (e) => {
     style: "decimal",
     useGrouping: !0
   }), y[o] = n), n.format(e);
-}, oe = (e) => e == null ? "" : e < 1e3 ? E(e) : e < 1e6 ? `${i(e / 1e3, 2, 0)}K` : e < 1e9 ? `${i(e / 1e6, 2, 0)}M` : e < 1e12 ? `${i(e / 1e9, 2, 0)}B` : `${i(e / 1e12, 2, 0)}T`, se = (e) => e == null ? "" : e === 1 ? "1 byte" : e < 1024 ? `${E(e)} bytes` : e < 1048576 ? `${i(e / 1024, 2, 0)} KB` : e < 1073741824 ? `${i(e / 1048576, 2, 0)} MB` : e < 1099511627776 ? `${i(e / 1073741824, 2, 0)} GB` : `${i(e / 1099511627776, 2, 0)} TB`, ie = (e) => e == null ? "" : e < 1e3 ? `${E(e)} ms` : e === 1e3 ? `${E(e)} sec` : e < 6e4 ? `${i(e / 1e3, 2, 0)} secs` : e === 6e4 ? "1 min" : e < 36e5 ? `${i(e / 6e4, 2, 0)} mins` : e === 36e5 ? "1 hr" : e < 864e5 ? `${i(e / 36e5, 2, 0)} hrs` : e === 864e5 ? "1 day" : `${i(e / 864e5, 2, 0)} days`, E = (e, t = R) => {
+}, ae = (e) => e == null ? "" : e < 1e3 ? E(e) : e < 1e6 ? `${i(e / 1e3, 2, 0)}K` : e < 1e9 ? `${i(e / 1e6, 2, 0)}M` : e < 1e12 ? `${i(e / 1e9, 2, 0)}B` : `${i(e / 1e12, 2, 0)}T`, ne = (e) => e == null ? "" : e === 1 ? "1 byte" : e < 1024 ? `${E(e)} bytes` : e < 1048576 ? `${i(e / 1024, 2, 0)} KB` : e < 1073741824 ? `${i(e / 1048576, 2, 0)} MB` : e < 1099511627776 ? `${i(e / 1073741824, 2, 0)} GB` : `${i(e / 1099511627776, 2, 0)} TB`, oe = (e) => e == null ? "" : e < 1e3 ? `${E(e)} ms` : e === 1e3 ? `${E(e)} sec` : e < 6e4 ? `${i(e / 1e3, 2, 0)} secs` : e === 6e4 ? "1 min" : e < 36e5 ? `${i(e / 6e4, 2, 0)} mins` : e === 36e5 ? "1 hr" : e < 864e5 ? `${i(e / 36e5, 2, 0)} hrs` : e === 864e5 ? "1 day" : `${i(e / 864e5, 2, 0)} days`, E = (e, t = R) => {
   if (e == null) return "";
   const a = `${t}decimal0.0`;
   let r = y[a];
@@ -271,7 +268,7 @@ const R = "en-US", y = {}, te = (e) => {
     style: "decimal",
     useGrouping: !0
   }), y[a] = r), r.format(e);
-}, le = (e) => {
+}, se = (e) => {
   switch (e) {
     case "csv":
       return "text/csv";
@@ -285,7 +282,7 @@ const R = "en-US", y = {}, te = (e) => {
     default:
       return "application/octet-stream";
   }
-}, L = [
+}, C = [
   { id: "dtv", label: { "en-gb": "Delimited Text" } },
   { id: "e/e", label: { "en-gb": "Entity/Event" } },
   { id: "jsonArray", label: { "en-gb": "JSON Array" } },
@@ -293,22 +290,22 @@ const R = "en-US", y = {}, te = (e) => {
   { id: "xls", label: { "en-gb": "XLS" } },
   { id: "xlsx", label: { "en-gb": "XLSX" } },
   { id: "xml", label: { "en-gb": "XML" } }
-], ce = (e = d) => {
+], ie = (e = d) => {
   const t = [];
-  for (const a of L) t.push({ ...a, label: a.label[e] || a.label[d] || a.id });
+  for (const a of C) t.push({ ...a, label: a.label[e] || a.label[d] || a.id });
   return t;
-}, $ = [
+}, _ = [
   { id: `
 `, label: { "en-gb": "Newline" } },
   { id: "\r", label: { "en-gb": "Carriage Return" } },
   { id: `\r
 `, label: { "en-gb": "Carriage Return/Newline" } }
-], de = (e = d) => {
+], le = (e = d) => {
   const t = [];
-  for (const a of $)
+  for (const a of _)
     t.push({ ...a, label: a.label[e] || a.label[d] || a.id });
   return t;
-}, M = [
+}, L = [
   { id: ":", label: { "en-gb": "Colon" } },
   { id: ",", label: { "en-gb": "Comma" } },
   { id: "!", label: { "en-gb": "Exclamation Mark" } },
@@ -320,12 +317,12 @@ const R = "en-US", y = {}, te = (e) => {
   { id: "_", label: { "en-gb": "Underscore" } },
   { id: "0x1F", label: { "en-gb": "Unit Separator" } },
   { id: "|", label: { "en-gb": "Vertical Bar" } }
-], me = (e = d) => {
+], ce = (e = d) => {
   const t = [];
-  for (const a of M)
+  for (const a of L)
     t.push({ ...a, label: a.label[e] || a.label[d] || a.id });
   return t;
-}, k = [
+}, $ = [
   { id: "alpha", color: "red", label: { "en-gb": "alpha" } },
   { id: "beta", color: "amber", label: { "en-gb": "beta" } },
   { id: "generalAvailability", color: "green", label: { "en-gb": "" } },
@@ -335,45 +332,45 @@ const R = "en-US", y = {}, te = (e) => {
   { id: "releaseCandidate", color: "green", label: { "en-gb": "release-candidate" } },
   { id: "unavailable", color: "other", label: { "en-gb": "unavailable" } },
   { id: "underReview", color: "other", label: { "en-gb": "under-review" } }
-], ue = (e, t = d) => {
-  const a = k.find((r) => r.id === e);
+], de = (e, t = d) => {
+  const a = $.find((r) => r.id === e);
   return a ? { ...a, label: a.label[t] || a.label[d] || e } : { id: e, color: "other", label: e };
 }, d = "en-gb";
 export {
-  G as APIError,
-  g as ApplicationError,
-  F as CONNECTOR_DESTINATION_OPERATIONS,
-  j as CONNECTOR_SOURCE_OPERATIONS,
+  V as APIError,
+  p as ApplicationError,
+  M as CONNECTOR_DESTINATION_OPERATIONS,
+  k as CONNECTOR_SOURCE_OPERATIONS,
   d as DEFAULT_LOCALE_CODE,
   H as DefaultTimestamp,
-  K as EngineError,
+  X as EngineError,
   v as FetchError,
-  Q as OperationalError,
-  _ as VueError,
-  q as WindowPromiseRejectionError,
-  J as WindowRuntimeError,
-  Y as buildFetchError,
-  Z as concatenateSerialisedErrorMessages,
-  z as convertMillisecondsToTimestamp,
-  te as convertODataTypeIdToUsageTypeId,
-  ne as extractExtensionFromPath,
-  ae as extractNameFromPath,
+  J as OperationalError,
+  O as VueError,
+  K as WindowPromiseRejectionError,
+  G as WindowRuntimeError,
+  q as buildFetchError,
+  Q as concatenateSerialisedErrorMessages,
+  P as convertMillisecondsToTimestamp,
+  ee as convertODataTypeIdToUsageTypeId,
+  te as extractExtensionFromPath,
+  re as extractNameFromPath,
   i as formatNumberAsDecimalNumber,
-  ie as formatNumberAsDuration,
-  oe as formatNumberAsSize,
-  se as formatNumberAsStorageSize,
+  oe as formatNumberAsDuration,
+  ae as formatNumberAsSize,
+  ne as formatNumberAsStorageSize,
   E as formatNumberAsWholeNumber,
-  ue as getComponentStatus,
-  V as getCurrentTimestamp,
-  ce as getDataFormats,
-  de as getRecordDelimiters,
-  me as getValueDelimiters,
-  le as lookupMimeTypeForExtension,
-  ee as normalizeToError,
-  X as presentationViewTypeMap,
-  re as serialiseError,
-  U as useCytoscapeJS,
-  B as useDataTable,
-  P as useHighcharts,
-  W as useMicromark
+  de as getComponentStatus,
+  W as getCurrentTimestamp,
+  ie as getDataFormats,
+  le as getRecordDelimiters,
+  ce as getValueDelimiters,
+  se as lookupMimeTypeForExtension,
+  Y as normalizeToError,
+  z as presentationViewTypeMap,
+  Z as serialiseError,
+  F as useCytoscapeJS,
+  U as useDataTable,
+  j as useHighcharts,
+  B as useMicromark
 };
